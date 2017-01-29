fd4f98d10493aff0689d2d175dc88b6f6f70b06e
Dony Benedict <dben22@gmail.com>
2017-01-29 20:22:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,33 +6,13 @@ const bodyParser = require('body-parser');
 
 const restService = express();
 restService.use(bodyParser.json());
-var eventschedule = {
-    slamNation:"SlamNation is happening on 3-02-2017 9 AM - 12 PM in Room 8",
-    youveGotMail:"You've Got Mail is happening on 3-02-2017 9 AM - 12 PM in Upper Seminar Room",
-    collegeGeneralQuiz :"College General Quiz prelims is happening on 3-02-2017 10 AM - 11 AM and finals on 3-02-2017 11 AM - 3 PM",
-    tarkvyuh:"Tarkvyuh is happening on 3-02-2017 9 AM - 5 PM",
-    spandan:"Spandan is happening on 3-02-2017 9 AM - 4 PM",
-    yavnika:"Yavnika is happening on 3-02-2017 9 AM - 2:30 PM",
-    izraz:"Izraz is happening on 3-02-2017 3 PM - 6 PM",
-    soloTrioCompetition:"Solo & Trio Competition is happening on 3-02-2017 9 AM - 12 PM",
-    malhaar:"Malhaar is happening on 3-02-2017 9 AM - 1 PM",
-    sugam:"Sugam is happening on 3-02-2017 2 PM - 5 PM",
-    kairosPhotoExhibition:"Kairos Photo Exhibition is happening on 3-02-2017 9 AM - 6 PM",
-    emakimono:"Emakimono is happening on 3-02-2017 10 AM - 2 PM",
-    spotOn:"Spot On is happening on 3-02-2017 2 PM - 3:30 PM",
-    musidora:"Musidora is happening on 3-02-2017 10 AM - 12 PM",
-    umbrellaPainting:"Umbrella Painting is happening on 3-02-2017 10 AM - 4 PM",
-    hiveexhibition :"Hive Exhibition is happening on 3-02-2017 7 AM - 8 PM",
-    jewellerymakingworkshop:"Jewellery Making Workshop is happening on 3-02-2017 9 AM - 4 PM",
-    tapestryandgreenquoteboard:"Tapestry and Green Quote Board is happening on 3-02-2017 9 AM - 5 PM"
-};
 
 restService.post('/hook', function (req, res) {
 
     console.log('hook request');
 
     try {
-        var speech = 'Talk to my Hand';
+        var speech = 'empty speech';
 
         if (req.body) {
             var requestBody = req.body;
@@ -46,7 +26,16 @@ restService.post('/hook', function (req, res) {
                 }
 
                 if (requestBody.result.action) {
-                    speech += 'action: ' + requestBody.result.action;
+                    speech += 'action: ' + requestBody.result.action + ' ';
+                }
+
+                var parameters = requestBody.result.parameters;
+                if (parameters){
+                    for (var p in parameters){
+                        if(parameters.hasOwnProperty(p) ) {
+                            speech += p + ": " + parameters[p] + "; ";
+                        }
+                    }
                 }
             }
         }
@@ -56,7 +45,7 @@ restService.post('/hook', function (req, res) {
         return res.json({
             speech: speech,
             displayText: speech,
-            source: 'GSGbot'
+            source: 'apiai-webhook-sample'
         });
     } catch (err) {
         console.error("Can't process request", err);
