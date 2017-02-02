4983f74031c4cbcb9b2f1bdd48bf817bcf7fea3a
Dony Benedict <dben22@gmail.com>
2017-02-02 23:40:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,29 +27,8 @@ var eventschedule = {
     jewellerymakingworkshop:"Jewellery Making Workshop is happening on 3-02-2017 9 AM - 4 PM",
     tapestryandgreenquoteboard:"Tapestry and Green Quote Board is happening on 3-02-2017 9 AM - 5 PM"
 };
-/*
-var eventschedule = {
-    slamNation:"SlamNation is happening on 3-02-2017 9 AM - 12 PM in Room 8",
-    youveGotMail:"You've Got Mail is happening on 3-02-2017 9 AM - 12 PM in Upper Seminar Room",
-    collegeGeneralQuiz :"College General Quiz prelims is happening on 3-02-2017 10 AM - 11 AM and finals on 3-02-2017 11 AM - 3 PM",
-    tarkvyuh:"Tarkvyuh is happening on 3-02-2017 9 AM - 5 PM",
-    spandan:"Spandan is happening on 3-02-2017 9 AM - 4 PM",
-    yavnika:"Yavnika is happening on 3-02-2017 9 AM - 2:30 PM",
-    izraz:"Izraz is happening on 3-02-2017 3 PM - 6 PM",
-    soloTrioCompetition:"Solo & Trio Competition is happening on 3-02-2017 9 AM - 12 PM",
-    malhaar:"Malhaar is happening on 3-02-2017 9 AM - 1 PM",
-    sugam:"Sugam is happening on 3-02-2017 2 PM - 5 PM",
-    kairosPhotoExhibition:"Kairos Photo Exhibition is happening on 3-02-2017 9 AM - 6 PM",
-    emakimono:"Emakimono is happening on 3-02-2017 10 AM - 2 PM",
-    spotOn:"Spot On is happening on 3-02-2017 2 PM - 3:30 PM",
-    musidora:"Musidora is happening on 3-02-2017 10 AM - 12 PM",
-    umbrellaPainting:"Umbrella Painting is happening on 3-02-2017 10 AM - 4 PM",
-    hiveexhibition :"Hive Exhibition is happening on 3-02-2017 7 AM - 8 PM",
-    jewellerymakingworkshop:"Jewellery Making Workshop is happening on 3-02-2017 9 AM - 4 PM",
-    tapestryandgreenquoteboard:"Tapestry and Green Quote Board is happening on 3-02-2017 9 AM - 5 PM"
-};*/
 
-var imageurl = "http://livon.allsocialassets.com/botimages";
+var imageurl = "http://livon.allsocialassets.com/botimages/";
 
 restService.post('/webhook', function (req, res) {
 
